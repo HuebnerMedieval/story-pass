bf25870109443ed234dcfe335e71601bf5e978ae
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2021-01-06 10:03:41 -0500

    removes unnecessary divs from index.html and creates basic function to get books

--- story-pass-frontend/index.js
+++ story-pass-frontend/index.js
@@ -1,1 +1,14 @@
 const BASE_URL = 'http://localhost:3000'
+
+window.addEventListener("DOMContentLoaded", () => {
+    getBooks()
+})
+
+const getBooks () {
+    let bookshelf = document.getElementById('bookshelf')
+    fetch(BASE_URL + '/books')
+    .then(res => res.json())
+    .then(books => {
+        console.log(books)
+    })
+}
