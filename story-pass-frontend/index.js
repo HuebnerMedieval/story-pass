a68f22de907fc9f2683c35aa3ee42e0c93353a52
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2021-01-06 14:59:09 -0500

    adds beginning code of function to show a single book

--- story-pass-frontend/index.js
+++ story-pass-frontend/index.js
@@ -6,15 +6,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
 const getBooks = () => {
     let bookshelf = document.getElementById('bookshelf')
+    bookshelf.innerHTML = ""
     fetch(BASE_URL + '/books')
     .then(res => res.json())
-    .then(books => books.map(book => {
-        bookshelf.innerHTML += `
-        <li>
-            <a href="#" data-id="${book.id}">${book.title}</a>
-            - ${book.finished ? "Finished" : "Work in Progress"}
-        </li>
-        `
-    }).join("")
-    )
+    .then(books => {
+        books.map(book => {
+            bookshelf.innerHTML += `
+            <li>
+                <a href="#" data-id="${book.id}">${book.title}</a>
+                - ${book.finished ? "Finished" : "Work in Progress"}
+            </li>
+            `
+        })
+        attachClicksToLinks()
+    })
+}
+
+function attachClicksToLinks() {
+    let books = document.querySelectorAll("li a")
+    books.forEach(book => {
+        book.addEventListener('click', displayBook)
+    })
+}
+
+function displayBook(e) {
+    let id = e.target.dataset.id
+
+    let bookshelf = document.getElementById('bookshelf')
+    bookshelf.innerHTML = ""
+
+    fetch(BASE_URL + `/books/${id}`)
+
 }
