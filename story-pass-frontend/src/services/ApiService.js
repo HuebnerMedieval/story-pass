f2bd3750baf86ed244d50c44ce1a09d0ff7f6ddb
Eli Huebner <elimhuebner@gmail.com>
2021-04-02 12:35:21 -0400

    add comments to ApiService.js

--- story-pass-frontend/src/services/ApiService.js
+++ story-pass-frontend/src/services/ApiService.js
@@ -1,22 +1,31 @@
 class ApiService {
+    // sets the default URL for API calls
     constructor(){
         this.baseURL = 'http://localhost:3000'
     }
 
+    // fetches book data from the backend
     async fetchBooks () {
+        // creates a promise object, resp, which holds the return from the fetch request to the Book index route
         let resp = await fetch(this.baseURL + '/books')
+        // converts resp into JSON and returns that JSON
         let data = await resp.json()
         return data
 
     }
 
+    // takes a book id and fetches that book's data from the backend
     async fetchBook(id){
+        // creates a promise object, resp, which holds the return from the fetch request to the Book show route
         let resp = await fetch(this.baseURL + `/books/${id}`)
+        // converts resp into JSON and returns that JSON
         let data = await resp.json()
         return data
     }
 
+    // sets a Post request to the backend to create a new book
     async fetchCreateBook(bookData){
+        // configuration object so fetch can send the post request
         let configObj = {
             method: 'POST',
             body: JSON.stringify(bookData),
@@ -26,14 +35,18 @@ class ApiService {
             }
         }
 
+        // fetch request with the configObj as a second argument to send the post request
         let resp = await fetch(this.baseURL + `/books`, configObj)
+        // converts the response into JSON and returns that JSON
         let data = await resp.json()
 
         return data
 
     }
 
+    // sets a Post request to the backend to create a new page
     async fetchCreatePage(pageData){
+        // configuration object so fetch can send the post request
         let configObj = {
             method: 'POST',
             body: JSON.stringify(pageData),
@@ -43,7 +56,9 @@ class ApiService {
             }
         }
 
+        // fetch request with the configObj as a second argument to send the post request
         let resp = await fetch(this.baseURL + '/pages', configObj)
+        // converts the response into JSON and returns that JSON
         let data = await resp.json()
 
         return data
