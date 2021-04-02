1a8051cc7fd09798fe69fdc27be708fc9ba38ede
Eli Huebner <elimhuebner@gmail.com>
2021-04-02 12:10:40 -0400

    adds comments to routes.rb

--- story-pass-backend/config/routes.rb
+++ story-pass-backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # enables create and show routes for pages
   resources :pages, only: [:create, :show]
+  # enables create, show, and index routes for books
   resources :books, only: [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
