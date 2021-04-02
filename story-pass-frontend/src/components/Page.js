4aa8deb60baa71f9e484eaf95948b8796a64bb23
Eli Huebner <elimhuebner@gmail.com>
2021-04-02 12:29:08 -0400

    adds comments to Page.js

--- story-pass-frontend/src/components/Page.js
+++ story-pass-frontend/src/components/Page.js
@@ -1,10 +1,12 @@
 class Page {
+    // instantiates a new Page, taking in an object and setting the values to the properties of the Page
     constructor(data) {
         this.id = data.id
         this.author = data.author
         this.content = data.content
     }
 
+    // takes in the page number and returns HTML displaying information about the Page
     renderPage(num){
         return `
         <h3>Page - ${num}</h3>
